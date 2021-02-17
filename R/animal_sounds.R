2cb04b78259b77483756bc6dbf18417cba56ad54
Stefanie Butland <stefaniebutland@gmail.com>
2021-02-17 07:16:24 -0800

    initial commit

---
+++ R/animal_sounds.R
@@ -0,0 +1,8 @@
+animal_sounds <- function(animal, sound) {
+  assertthat::assert_that(
+    assertthat::is.string(animal),
+    assertthat::is.string(sound)
+  )
+  paste0("The ", animal, " farts ", sound, "!")
+}
+
